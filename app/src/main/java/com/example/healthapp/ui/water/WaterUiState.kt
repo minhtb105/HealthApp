3b90952feee49c88ab029a6167409da61623485f
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-12-19 14:40:58 +0700

    feat: introduce WaterUiState to represent UI states in water feature

--- app/src/main/java/com/example/healthapp/ui/water/WaterUiState.kt
+++ app/src/main/java/com/example/healthapp/ui/water/WaterUiState.kt
@@ -1,4 +1,20 @@
 package com.example.healthapp.ui.water
 
-class WaterUiState {
+import com.example.healthapp.domain.model.WaterIntake
+
+
+sealed class WaterUiState {
+
+    object Loading : WaterUiState()
+
+    data class Success(
+        val totalAmountMl: Int,
+        val items: List<WaterIntake>
+    ) : WaterUiState()
+
+    object Empty : WaterUiState()
+
+    data class Error(
+        val message: String
+    ) : WaterUiState()
 }
