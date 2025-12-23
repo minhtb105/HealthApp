7456084fc2bdc43fa1b12af537e9f1baf7ecc7a5
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-12-23 21:49:51 +0700

    Add HealthApplication with @HiltAndroidApp

--- app/src/main/java/com/example/healthapp/HealthApplication.kt
+++ app/src/main/java/com/example/healthapp/HealthApplication.kt
@@ -1,5 +1,8 @@
 package com.example.healthapp
 
-class HealthApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-}
+
+@HiltAndroidApp
+class HealthApplication : Application()
