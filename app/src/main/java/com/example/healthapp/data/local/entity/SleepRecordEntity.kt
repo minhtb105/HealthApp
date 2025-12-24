f855916a1343dc75c5a76f5f2c8fdadc34661426
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-12-24 18:03:09 +0700

    feat: add SleepEntity for local storage

---
+++ app/src/main/java/com/example/healthapp/data/local/entity/SleepRecordEntity.kt
@@ -0,0 +1,15 @@
+package com.example.healthapp.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "sleep_records")
+data class SleepRecordEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val sleepTime: Long,
+    val wakeTime: Long,
+    val duration: Long,
+    val date: String
+)
